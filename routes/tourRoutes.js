99e03b40a94413faec2655996be4bcc098df8c8b
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-08-28 02:42:31 +0530

    Restructure project: split app.js and server.js, clean routes/controllers

---
+++ routes/tourRoutes.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const tourController = require('./../controllers/tourController.js');
+
+const express = require('express');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
+);
+
+const router = express.Router();
+router
+  .route('/')
+  .get(tourController.getAllTours)
+  .post(tourController.createTour);
+router
+  .route('/:id')
+  .get(tourController.getTour)
+  .patch(tourController.updateTour)
+  .delete(tourController.deleteTour);
+module.exports = router;
