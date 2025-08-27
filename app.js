1b0977e7ea04c537888651750ddc7748b96d710f
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-08-27 20:41:42 +0530

    Creating Own Middleware

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
 const fs = require('fs');
 const express = require('express');
 const app = express();
-app.use(express.json());
 const port = 3000;
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log('hello from MiddleWare');
+  next();
+});
+app.use((req, res, next) => {
+  req.requestTime = new Date().toISOString();
+  next();
+});
+
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 const getAllTours = (req, res) => {
+  console.log(req.requestTime);
   res.status(200).json({
     status: 'success',
+    requestedAt:req.requestTime,
     results: tours.length,
     data: {
       tours,
