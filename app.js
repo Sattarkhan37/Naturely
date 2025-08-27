121158dfc2fcbdf514de2949b0078cbe52c8345a
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-08-27 16:58:44 +0530

    Responding to URL Parameters

--- app.js
+++ app.js
@@ -8,10 +8,29 @@ const tours = JSON.parse(
 );
 app.get('/api/v1/tours', (req, res) => {
   res.status(200).json({
-    status: 'sucess',
+    status: 'success',
     results: tours.length,
     data: {
       tours,
+    },
+  });
+});
+app.get('/api/v1/tours/:id', (req, res) => {
+  console.log(req.params);
+  const id = req.params.id * 1;
+  const tour = tours.find((el) => el.id === id);
+  // if (id > tours.length)
+  if (!tour) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour,
     },
   });
 });
@@ -26,7 +45,7 @@ app.post('/api/v1/tours', (req, res) => {
     JSON.stringify(tours),
     (err) => {
       res.status(201).json({
-        status: 'sucess',
+        status: 'success',
         data: {
           tour: newTour,
         },
