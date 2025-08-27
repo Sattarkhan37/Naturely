60df521b28f5c2e1e4f339369262f1de81b97c28
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-08-27 17:18:02 +0530

    Handling DELETE Requests

--- app.js
+++ app.js
@@ -67,6 +67,18 @@ app.patch('/api/v1/tours/:id', (req, res) => {
     },
   });
 });
+app.delete('/api/v1/tours/:id', (req, res) => {
+  if (req.params.id * 1 > tours.length) {
+    return res.status(404).json({
+      status: 'fail',
+      message: 'Invalid Id',
+    });
+  }
+  res.status(204).json({
+    status: 'success',
+    data: null,
+  });
+});
 app.listen(port, () => {
   console.log('App Running on Port 3000');
 });
