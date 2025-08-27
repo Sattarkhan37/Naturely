ca1f2584af7031cdf5ce00082c46aef6f53a7687
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-08-27 16:13:14 +0530

    Handling Get request

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ message: 'Hello From the Server Side', app: 'natours' });
-});
-app.post('/', (req, res) => {
-  res.send('you can post to this endpoint');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status:"sucess",
+    results:tours.length,
+    data:{
+      tours
+    }
+  })
 });
 app.listen(port, () => {
   console.log('App Running on Port 3000');
