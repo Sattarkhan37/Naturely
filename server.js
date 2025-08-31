5e221a4fffcc415107fe51d5911ca3d2260ff530
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-08-31 16:33:14 +0530

    Creating Documents and Testing the Model

--- server.js
+++ server.js
@@ -20,18 +20,31 @@ const tourSchema = new mongoose.Schema({
   name: {
     type: String,
     required: [true, 'A tour must have a name'],
-    unique:[true, 'name must be unique']
+    unique: [true, 'name must be unique'],
   },
   rating: {
     type: Number,
-    default: 4.5
+    default: 4.5,
   },
   price: {
     type: Number,
     required: [true, 'A tour must have a price'],
   },
 });
-const Tour = mongoose.model('Tour',tourSchema)
+const Tour = mongoose.model('Tour', tourSchema);
+const testTour = new Tour({
+  name: 'the Park Hiker',
+  rating: 4.7,
+  price: 300,
+});
+testTour
+  .save()
+  .then((doc) => {
+    console.log(doc);
+  })
+  .catch((err) => {
+    console.log('ERROR:', err);
+  });
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('App Running on Port 3000');
