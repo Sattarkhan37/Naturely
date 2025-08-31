02124c7ee383edd060dc727b4f7aed2332b35721
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-08-31 16:14:48 +0530

    feat: create Tour model with basic fields and validation

--- server.js
+++ server.js
@@ -16,6 +16,22 @@ mongoose
     console.log('DB SUCCESS');
   });
 // console.log(process.env);
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique:[true, 'name must be unique']
+  },
+  rating: {
+    type: Number,
+    default: 4.5
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+const Tour = mongoose.model('Tour',tourSchema)
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log('App Running on Port 3000');
