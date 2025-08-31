dae2fb935315212c351315e29b76512b3c9ef7a1
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-09-01 03:56:52 +0530

    feat(api): add advanced filtering with operators

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,14 +1,19 @@
 const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
+    //1)filtering
     const queryObj = { ...req.query };
     const excludedFields = ['pages', 'sort', 'limit', 'fields'];
     excludedFields.forEach((el) => {
       delete queryObj[el];
     });
     console.log(req.query, queryObj);
+    //2)Advance filtering
+    let queryStr = JSON.stringify(queryObj);
+    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
+    console.log(JSON.parse(queryStr));
 
-    const query = Tour.find(queryObj);
+    const query = Tour.find(JSON.parse(queryStr));
     const tours = await query;
     res.status(200).json({
       status: 'success',
