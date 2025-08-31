1cf76912840bf574028906fff028436f1a4063b2
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-09-01 02:46:07 +0530

    feat(api): add basic filtering functionality

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,7 +1,15 @@
 const Tour = require('./../models/tourModel');
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    const queryObj = { ...req.query };
+    const excludedFields = ['pages', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => {
+      delete queryObj[el];
+    });
+    console.log(req.query, queryObj);
+
+    const query = Tour.find(queryObj);
+    const tours = await query;
     res.status(200).json({
       status: 'success',
       results: tours.length,
