07bb8e4b993abad8b2f9f1d05466b1c902f2a359
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-09-02 03:07:40 +0530

    Implement built-in validators (required, minlength, maxlength, enum, min, max)

--- models/tourModel.js
+++ models/tourModel.js
@@ -7,6 +7,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters'],
     },
     slug: String,
     duration: {
@@ -20,10 +22,16 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'difficulty is either easy , medium or difficult ',
+      },
     },
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'rating must be above 1.0'],
+      max: [5, 'rating must be below 5.0'],
     },
     ratingsQuantity: {
       type: Number,
