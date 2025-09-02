aba0b307bec9b5c5298e7d8caa07988a858507a0
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-09-02 20:42:26 +0530

    feat(api): return user-friendly message for invalid database ObjectId

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,3 +1,9 @@
+const AppError = require('../utils/appError');
+
+const handelCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}:${err.value}.`;
+  return new AppError(message, 404);
+};
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -31,6 +37,8 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === 'development') {
     sendErrorDev(err, res);
   } else if (process.env.NODE_ENV === 'production') {
-    sendErrorProd(err, res);
+    let error = err;
+    if (error.name === 'CastError') error = handelCastErrorDB(error);
+    sendErrorProd(error, res);
   }
 };
