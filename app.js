764aa0665f9ad69b99c3c2ff58570091864fb707
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-09-02 15:33:48 +0530

    Handling Unhandled Routes

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ if (process.env.NODE_ENV === 'development') {
 
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
-app.use((req, res, next) => {
-  console.log('hello from MiddleWare');
-  next();
-});
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -24,8 +20,16 @@ app.use((req, res, next) => {
 // app.get('/api/v1/tours/:id', getTour);
 // app.patch('/api/v1/tours/:id', updateTour);
 // app.delete('/api/v1/tours/:id', deleteTour);
+
 // 3)Routes
 
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server`,
+  });
+});
 module.exports = app;
