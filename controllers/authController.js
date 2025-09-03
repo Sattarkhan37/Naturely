62e56ea2aeff4beec761b1e48a5ed4a24fe8eb60
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-09-04 02:32:00 +0530

    fix(user): restrict fields allowed during signup (no extra fields)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,13 @@
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync.js');
 exports.signup = catchAsync(async (req, res, next) => {
-  const newUser = await User.create(req.body);
+  const newUser = await User.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+    passwordConfirm: req.body.passwordConfirm,
+  });
+  newUser.password = undefined;
   res.status(201).json({
     data: {
       user: newUser,
