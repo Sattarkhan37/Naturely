faff2d1485c6856356d71abdddc250917722a33e
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-09-04 03:49:30 +0530

    feat(auth): implement user signup and login with JWT

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,15 @@
-exports.getAllUsers = (req, res) => {
-  res.status(500).json({
-    status: 'error',
-    message: 'This route is not yet defined',
+const User = require('./../models/userModel.js');
+const catchAsync = require('./../utils/catchAsync.js');
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const user = await User.find();
+  res.status(200).json({
+    status: 'success',
+    results: user.length,
+    data: {
+      user,
+    },
   });
-};
+});
 exports.getUser = (req, res) => {
   res.status(500).json({
     status: 'error',
