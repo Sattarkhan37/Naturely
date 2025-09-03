c933535dd97faaff0d01590897d9d5cd8c990b56
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-09-03 18:13:50 +0530

    feat(error): handle mongoose validation errors gracefully

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -9,6 +9,11 @@ const handelDuplicateFieldsDB = (err) => {
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
 };
+const handelValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
     status: err.status,
@@ -45,6 +50,8 @@ module.exports = (err, req, res, next) => {
     let error = err;
     if (error.name === 'CastError') error = handelCastErrorDB(error);
     if (error.code === 11000) error = handelDuplicateFieldsDB(error);
+    if (error.name === 'ValidationError')
+      error = handelValidationErrorDB(error);
     sendErrorProd(error, res);
   }
 };
