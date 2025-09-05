87d01b2cd690d0659d0b093d44ed22d1c61b4614
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-09-06 01:08:06 +0530

    Deleting the Current User

--- models/userModel.js
+++ models/userModel.js
@@ -39,6 +39,11 @@ const userSchema = new mongoose.Schema({
   passwordChangedAt: { type: Date },
   passwordResetToken: String,
   passwordResetExpires: Date,
+  active: {
+    type: Boolean,
+    default: true,
+    select: false,
+  },
 });
 userSchema.pre('save', async function (next) {
   if (!this.isModified('password')) return next();
@@ -50,6 +55,10 @@ userSchema.pre('save', async function (next) {
 userSchema.pre('save', function (next) {
   if (!this.isModified('password') || this.isNew) return next();
   this.passwordChangedAt = Date.now() - 1000;
+  next();
+});
+userSchema.pre(/^find/, function (next) {
+  this.find({ active: { $ne: false } });
   next();
 });
 userSchema.methods.correctPassword = async function (
