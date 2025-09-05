38e67ea0744d7ecbeacd6f2083ffa3aa4c5555e5
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-09-06 02:46:57 +0530

    feat(security): set security HTTP headers using helmet

--- app.js
+++ app.js
@@ -2,24 +2,29 @@ const express = require('express');
 const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const golbalErorrHandler = require('./controllers/errorController');
 const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
-//1) Middleware
+// set security HTTP headers
+app.use(helmet());
+//Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-
+// Limit requests from same API
 const limiter = rateLimit({
   max: 500,
   windowMs: 60 * 60 * 1000,
   message: 'T00 Many request from this IP, please try again in an hour',
 });
 app.use('/api', limiter);
-app.use(express.json());
+//Body parser,reading data from thebody into req.body
+app.use(express.json({ limit: '10kb' }));
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
-
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
