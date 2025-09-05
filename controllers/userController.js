0c7b25f85781012bd79634ddf4464ed116fcf71b
Sattarkhan37 <abdulsattarkhan973@gmail.com>
2025-09-05 21:19:11 +0530

    feat(users): allow logged-in user to update their own name and email

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,13 @@
+const AppError = require('../utils/appError.js');
 const User = require('./../models/userModel.js');
 const catchAsync = require('./../utils/catchAsync.js');
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+  return newObj;
+};
 exports.getAllUsers = catchAsync(async (req, res, next) => {
   const user = await User.find();
   res.status(200).json({
@@ -7,6 +15,30 @@ exports.getAllUsers = catchAsync(async (req, res, next) => {
     results: user.length,
     data: {
       user,
+    },
+  });
+});
+exports.updateMe = catchAsync(async (req, res, next) => {
+  //1) Create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password update please use updateMyPassword ',
+        400,
+      ),
+    );
+  }
+  //2)Filtered out unwanted field names that are mot allowed to be updated
+  const filteredBody = filterObj(req.body, 'name', 'email');
+  //3)update user doc
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   });
 });
